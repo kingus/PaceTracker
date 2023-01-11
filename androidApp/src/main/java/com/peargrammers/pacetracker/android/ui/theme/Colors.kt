63114eb735a409602400d60249de1ba26c7e085c
kingus <kingulka.a@gmail.com>
2023-01-11 23:18:23 +0100

    [android] Add basic colors in Colors file

---
+++ androidApp/src/main/java/com/peargrammers/pacetracker/android/ui/theme/Colors.kt
@@ -0,0 +1,24 @@
+package com.peargrammers.pacetracker.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xffed9b40)
+val DarkPrimaryColor = Color(0xFFFA901B)
+
+val TextPrimary = Color(0xFF161616)
+val DarkTextPrimary = Color(0xFFF8F8F8)
+
+val TextSecondary = Color(0xFF5C5C5C)
+val DarkTextSecondary = Color(0xFFC3C3C3)
+
+
+val LightOrange = Color(0xffffe5be)
+val IntenseOrange = Color(0xffff9a00)
+
+val DarkBlue = Color(0xff000c30)
+val LightBlue = Color(0xffbef0ff)
+val IntenseBlue = Color(0xff00c3fd)
+val MediumGrey = Color(0xffdadae6)
+val LightGrey = Color(0xFFF6F6FC)
+val Red = Color(0xFFF10000)
+val LightRed = Color(0xFFFF7E7E)
