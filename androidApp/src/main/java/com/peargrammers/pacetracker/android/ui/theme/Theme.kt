56f1703e9554c3e1d9110aa35df53ce39e8a86d3
kingus <kingulka.a@gmail.com>
2023-01-11 23:28:35 +0100

    [android] Add Theme file

---
+++ androidApp/src/main/java/com/peargrammers/pacetracker/android/ui/theme/Theme.kt
@@ -0,0 +1,42 @@
+package com.peargrammers.pacetracker.android.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+
+private val DarkColorPalette = darkColors(
+    primary = DarkPrimaryColor, primaryVariant = DarkTextPrimary, secondary = DarkTextSecondary
+)
+
+private val LightColorPalette = lightColors(
+    primary = PrimaryColor, primaryVariant = TextPrimary, secondary = TextSecondary
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun PaceTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+
+    }
+
+    MaterialTheme(
+        colors = colors, typography = Typography, shapes = Shapes, content = content
+    )
+}
